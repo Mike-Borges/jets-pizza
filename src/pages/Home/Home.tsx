a00cb4bf951b4b99771cc59d2132d8322ecdd866
Mike Borges <mikeaborges29@gmail.com>
2026-05-02 13:04:00 -0600

    [feat]Home animation and layout fixed for first section.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,6 @@
 import Header from '../../components/Header/Header';
+import heroGif from '../../assets/jet-home-ani.gif';
+import heroGifBorder from '../../assets/jet-home-ani-bx.png';
 import styles from './Home.module.css';
 
 export default function Home() {
@@ -7,14 +9,18 @@ export default function Home() {
       <Header />
       <section className={styles.homeHero}>
         <div className={styles.homeTitleBx}>
-         <h1 className={styles.homeTitle}>
-  <span className={styles.homeTitleOrder}>ORDER</span>
-  <span className={styles.homeTitleFresh}>FRESH PIZZA</span>
-  <span className={styles.homeTitleNear}>NEAR YOU!</span>
-</h1>
+          <h1 className={styles.homeTitle}>
+            <span className={styles.homeTitleOrder}>ORDER</span>
+            <span className={styles.homeTitleFresh}>FRESH PIZZA</span>
+            <span className={styles.homeTitleNear}>NEAR YOU!</span>
+          </h1>
           <label className={styles.homeLocationTitle}>Find Your Nearest Location:
-          <input type="text" className={styles.homeLocationText} placeholder="Enter city, state, or ZIP" />
-       </label>
+            <input type="text" className={styles.homeLocationText} placeholder="Enter city, state, or ZIP" />
+          </label>
+        </div>
+        <img src={heroGifBorder} alt="" className={styles.homeHeroGreen} />
+        <div className={styles.homeHeroImgBx}>
+          <img src={heroGif} alt="Pizza Hero" className={styles.homeHeroImg} />
         </div>
       </section>
     </div>
