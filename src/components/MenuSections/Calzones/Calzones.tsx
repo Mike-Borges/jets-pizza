adeedd6f2714782c01b5b3fae29d19884c4f1666
Mike Borges <mikeaborges29@gmail.com>
2026-05-15 16:10:25 -0600

    [feat]Rest of menu sections added. Still need to style pages, add proper images, and add footer

--- src/components/MenuSections/Calzones/Calzones.tsx
+++ src/components/MenuSections/Calzones/Calzones.tsx
@@ -1,3 +1,30 @@
+// Calzones.tsx
+import MenuCard from '../../MenuCard/MenuCard';
+import styles from './Calzones.module.css';
+
+const calzones = [
+  { id: 1, name: 'Jet\'s Calzone', price: '$10.00', image: '' },
+  { id: 2, name: 'Pepperoni Calzone', price: '$10.00', image: '' },
+  { id: 3, name: 'Supreme Calzone', price: '$10.00', image: '' },
+  { id: 4, name: 'Meaty Calzone', price: '$10.00', image: '' },
+];
+
 export default function Calzones() {
-  return <div>Calzones</div>;
+  return (
+    <div className={styles.calzones}>
+      <section className={styles.calzonesSection}>
+        <h2 className={styles.calzonesSectionTitle}>Calzones</h2>
+        <div className={styles.calzonesGrid}>
+          {calzones.map((item) => (
+            <MenuCard
+              key={item.id}
+              name={item.name}
+              price={item.price}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
